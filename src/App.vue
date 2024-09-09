808f351ee712427b55ac5fc19479eaef297ba5e8
Rodrigo Rocha <109789436+RodrigoRocha10@users.noreply.github.com>
2024-09-09 18:11:02 +0100

    init 1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: 'Rodrigo Rocha',
-      };
-  },
-};
+<script setup>
+import {ref} from 'vue';
+
+    const name = ref('Rodrigo Rocha');
+    const status = ref('active');
+    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+    const toggleStatus = () => {
+      if(status.value === 'active') {
+        status.value = 'pending';
+      } else if(status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+    };
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    };
+
 </script>
 
 <template>
-  <h1>Vue Jobs</h1>
+  <h1>{{name}}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <br>
+  <button @click="toggleStatus">Change Status</button>
 </template>
