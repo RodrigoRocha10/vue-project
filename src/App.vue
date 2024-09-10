9ed5e58f29b3a92ed53b93547f79123f82e09988
Rodrigo Rocha <109789436+RodrigoRocha10@users.noreply.github.com>
2024-09-10 11:37:09 +0100

    update #1

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,9 @@
 <script setup>
-import {ref} from 'vue';
-
-    const name = ref('Rodrigo Rocha');
-    const status = ref('active');
-    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
-
-    const toggleStatus = () => {
-      if(status.value === 'active') {
-        status.value = 'pending';
-      } else if(status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    };
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    };
-
+import Navbar from '@/components/Navbar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <h1>{{name}}</h1>
-  <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-  <br>
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
+  <RouterView />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
